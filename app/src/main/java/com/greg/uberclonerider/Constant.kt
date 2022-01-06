2d57f6a8675fe2bed5d780c00aaffe57b0c52e48
GregBiglione <gregory.biglione81@gmail.com>
2022-01-06 02:52:29 +0100

    Added Maps in HomeFragment, drawer_item_color_stat & map style

--- app/src/main/java/com/greg/uberclonerider/Constant.kt
+++ app/src/main/java/com/greg/uberclonerider/Constant.kt
@@ -4,5 +4,6 @@ class Constant {
     companion object{
         const val LOGIN_REQUEST_CODE = 2108
         const val RIDER_INFORMATION = "RiderInformation"
+        const val DEFAULT_ZOOM = 17.0f
     }
 }
--- app/src/main/java/com/greg/uberclonerider/ui/home/HomeFragment.kt
+++ app/src/main/java/com/greg/uberclonerider/ui/home/HomeFragment.kt
@@ -4,28 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.greg.uberclonerider.Constant.Companion.DEFAULT_ZOOM
 import com.greg.uberclonerider.R
+import com.greg.uberclonerider.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var map: GoogleMap
+    //------------------- Location -----------------------------------------------------------------
+    private lateinit var rioDeJaneiro: LatLng
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(callback)
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Show map when ready & add a maker ----------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        rioDeJaneiro = LatLng(-22.933988, -43.171960)
+        map = googleMap
+        googleMap.addMarker(MarkerOptions()
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            .position(rioDeJaneiro).title(getString(R.string.user_position_message))
+        )
+        moveCamera()
+        zoomOnLocation()
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Move camera --------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun moveCamera() {
+        map.moveCamera(CameraUpdateFactory.newLatLng(rioDeJaneiro))
+    }
+
+    //----------------------------------------------------------------------------------------------
+    //------------------- Zoom level ---------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+    private fun zoomOnLocation(){
+        map.animateCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM))
     }
 }
